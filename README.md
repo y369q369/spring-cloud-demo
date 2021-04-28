8844914e9c66ff1306f77d3db535ab4c5c047a01
y369q369 <1486866853@qq.com>
2021-04-28 17:11:48 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# spring-cloud-demo
+
+- 参考
+
+  > 1. 视频：https://www.bilibili.com/video/BV19C4y187Ar?p=4
+  > 2. 官网：https://spring.io/projects/spring-cloud
+  > 3. 书籍：《SpringCloud微服务实战.pdf》
---
+++ eureka/src/main/java/gs/demo/EurekaApplication.java
@@ -0,0 +1,21 @@
+package gs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author grassPrince
+ * @Date 2021/4/28 16:27
+ * @Description TODO
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+
+}
